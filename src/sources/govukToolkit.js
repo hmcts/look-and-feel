a3564fe481b4e7907c06dcab5c647683056b0a77
Michael Allen <michael@allen.digital>
2017-08-29 17:10:14 +0100

    Ensure govuk toolkit images are available in the webpack output

--- src/sources/govukToolkit.js
+++ src/sources/govukToolkit.js
@@ -1,7 +1,18 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const packageJson = require.resolve('govuk_frontend_toolkit/package.json');
 const root = path.resolve(packageJson, '..');
 const sass = path.resolve(root, 'stylesheets');
+const images = path.resolve(root, 'images');
+const javascripts = path.resolve(root, 'javascripts');
 
-module.exports = { paths: { root, sass } };
+const copyGovukToolkitAssets = new CopyWebpackPlugin([
+  { from: images, to: 'images' },
+  { from: javascripts, to: 'javascripts' }
+]);
+
+module.exports = {
+  paths: { root, sass },
+  plugins: [ copyGovukToolkitAssets ]
+};
