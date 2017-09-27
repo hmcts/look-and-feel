bb64a301185b22e3340e6275ca6af246a6d4a163
Michael Allen <michael@michaelallen.io>
2017-09-27 14:48:26 +0100

    Fix linting errors

--- src/sources/javascript.js
+++ src/sources/javascript.js
@@ -1,27 +1,26 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const extractJSFiles = (files) => {
-    return files.filter( file => file.endsWith('.js') )
+const extractJSFiles = files => files.filter(file => file.endsWith('.js'));
+
+const assetPathOnwards = files => {
+  const insideAssets = file => file.substring(file.search('assets'));
+  return files.map(insideAssets);
 };
 
-const assetPathOnwards = (files) => {
-    return files.map( file => file.substring(file.search('assets')))
-};
+const createFromToStructure = jsFiles => jsFiles.map(file => {
+  return { from: file, to: 'javascripts' };
+});
 
-const createFromToStructure = (jsFiles) => {
-    return jsFiles.map( file => ({ from: file, to: 'javascripts' }) );
-};
-
-const copyJavaScriptToWebpack = (files) => {
-    const jsFiles = extractJSFiles(files);
-    const jsFilesAssetPath = assetPathOnwards(jsFiles);
-    const fromToStructure = createFromToStructure(jsFilesAssetPath);
-    return { plugins: [new CopyWebpackPlugin( fromToStructure )] };
+const copyJavaScriptToWebpack = files => {
+  const jsFiles = extractJSFiles(files);
+  const jsFilesAssetPath = assetPathOnwards(jsFiles);
+  const fromToStructure = createFromToStructure(jsFilesAssetPath);
+  return { plugins: [new CopyWebpackPlugin(fromToStructure)] };
 };
 
 module.exports = {
-    extractJSFiles,
-    assetPathOnwards,
-    createFromToStructure,
-    copyJavaScriptToWebpack,
+  extractJSFiles,
+  assetPathOnwards,
+  createFromToStructure,
+  copyJavaScriptToWebpack
 };
