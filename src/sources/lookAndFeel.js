25656b9f439fbc2d39ab48a7a960d1e9f5791f6b
Michael Allen <michael@michaelallen.io>
2017-11-02 09:48:34 +0000

    Add sass for check your answers

--- src/sources/lookAndFeel.js
+++ src/sources/lookAndFeel.js
@@ -3,8 +3,9 @@ const path = require('path');
 const root = path.resolve(__dirname, './../../');
 const templates = path.resolve(root, './templates');
 const oldTemplates = path.resolve(root, './templates/look-and-feel');
+const sass = path.resolve(root, './assets/scss');
 
 module.exports = {
-  paths: { root, templates },
+  paths: { root, templates, sass },
   backwardsCompatibility: { templates: oldTemplates }
 };
