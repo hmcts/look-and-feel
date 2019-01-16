edbd38329e575c704bc2735d42fc540970fdb78c
Dominic Taylor <dom.taylor7@gmail.com>
2019-01-16 10:09:10 +0000

    add progress list

--- src/sources/lookAndFeel.js
+++ src/sources/lookAndFeel.js
@@ -1,11 +1,18 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const root = path.resolve(__dirname, './../../');
 const templates = path.resolve(root, './templates');
 const oldTemplates = path.resolve(root, './templates/look-and-feel');
 const sass = path.resolve(root, './assets/scss');
+const images = path.resolve(root, './assets/images');
+
+const copyLookAndFeelAssets = new CopyWebpackPlugin(
+  [{ from: images, to: 'images' }]
+);
 
 module.exports = {
   paths: { root, templates, sass },
+  plugins: [ copyLookAndFeelAssets ],
   backwardsCompatibility: { templates: oldTemplates }
 };
