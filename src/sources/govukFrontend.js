2d5ef709c1247ccbc260440b22b057e3453d23ed
Hosain <hosain.alahmad@hmcts.net>
2019-02-04 15:17:23 +0000

    DIV-3893 - update to govuk-frontend styles
    Mostly followed the guide on
    https://design-system.service.gov.uk/get-started/updating-your-code/

--- src/sources/govukFrontend.js
+++ src/sources/govukFrontend.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 const packageJson = require.resolve('govuk-frontend/package.json');
 const root = path.resolve(packageJson, '..');
+const scss = path.resolve(root, 'all.scss');
+const javascript = path.resolve(root, 'all.js');
 const components = path.resolve(root, 'components');
 
-module.exports = { paths: { templates: root, components, sass: root, javascript: root } };
+module.exports = { paths: { templates: root, components, scss, javascript } };
